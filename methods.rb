232ffaea9890ce84e198c7b56580b05b6e41e1a9
Kyrian <dr.kyrian.onwe@gmail.com>
2022-07-04 00:16:37 -0500

    commit tree 61e5576476623368260efbdd2ab7e645e1745a50

--- methods.rb
+++ methods.rb
@@ -1,1 +1,33 @@
 # Your code here!
+def my_function param
+    puts "Running myFunction in Ruby"
+    param + 1
+end
+
+my_function_return_value = my_function(1)
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet name 
+    puts "Hello, #{name}!" 
+end
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add num1, num2
+    puts num1 + num2
+    num1 + num2
+end
+
+def halve number
+    if number != number.to_i
+        nil
+    else 
+        number / 2
+    end
+end
+  
